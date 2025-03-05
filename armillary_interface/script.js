343b40190fd3aebb9bd53cea965ebaad71ea7cf1
Atis Yosprakob <yosprakob2@gmail.com>
2025-03-06 07:36:14 +0900

    Add files via upload

--- armillary_interface/script.js
+++ armillary_interface/script.js
@@ -1,13 +1,15 @@
 
 function renderMath() {
     let input = document.getElementById("latexInput").value;
-    input = latexReformat(input)
-    if(input[0]=="+"){
-        input = input.replace("+","")
+    if(input.length>0){
+        input = latexReformat(input)
+        if(input[0]=="+"){
+            input = input.replace("+","")
+        }
+        document.getElementById("output1").innerHTML = "<div class=\"wrap\">\\[" + input + "\\]</div>";
+        MathJax.typesetPromise();
+        countUOccurrences();
     }
-    document.getElementById("output1").innerHTML = "<div class=\"wrap\">\\[" + input + "\\]</div>";
-    MathJax.typesetPromise();
-    countUOccurrences();
 }
 
 function applyPresets() {
@@ -16,7 +18,7 @@ function applyPresets() {
     let presetText = "";
     
     checkboxes.forEach(box => {
-        presetText += box.value + "\n";
+        presetText += box.value + "\n\n";
     });
     
     textArea.value = presetText;
@@ -29,6 +31,8 @@ function adjustHeight(textarea) {
 }
 
 function countUOccurrences() {
+    document.getElementById("output2").innerHTML = "";
+    document.getElementById("interlude1to2").innerHTML = "";
 
     let display = ""
 
@@ -37,18 +41,73 @@ function countUOccurrences() {
 
     latexExpr = latexExpr.split(/[\+\-]/)
 
-    let term = 1
-    for(const expression of latexExpr){
-        let result = parseOneTerm(expression)
-        if(result==="")
-            continue
-        //display += "<b>Term "+term+"</b>: \\["+expression+"\\]<br>"
-        display += "<h4>$\\bullet$ Term "+term+"</h4>"
-        display += result
-        term+=1
+    function ordinal(x) {
+        if (typeof x !== "number" || !Number.isInteger(x)) {
+            throw new Error("Input must be an integer.");
+        }
+
+        let suffix = "th";
+        if (x % 100 < 11 || x % 100 > 13) {
+            switch (x % 10) {
+                case 1: suffix = "st"; break;
+                case 2: suffix = "nd"; break;
+                case 3: suffix = "rd"; break;
+            }
+        }
+
+        return x + suffix;
     }
 
-    document.getElementById("output2").innerHTML = display;
+    let term = 1
+    let linkCount = []
+    let parsed_operator = []
+    for(const expression of latexExpr){
+        let result = parseOneTerm(expression)
+        for(let key in result[1]){
+            if(key in linkCount)
+                linkCount[key] += result[1][key]
+            else
+                linkCount[key] = result[1][key]
+            
+        }
+        if(result[0]==="")
+            continue
+        display += ("<h4>$\\bullet$ "+ordinal(term)+" term "
+            +"<font color='#888'>"
+            +"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+            +"</font>"
+            +"</h4>")
+        if(parsed_operator.includes(result[2]))
+            display+= "<font color='red'>Note: this term can and should be merged with one of the previous terms.</font><br>"
+        display += result[0]
+        parsed_operator.push(result[2])
+        term+=1
+    }
+    if(display!=""){
+        document.getElementById("interlude1to2").innerHTML = "<center><img src='images/downarrow.jpg' width='10%'></center>";
+        display += ("<h4>$\\bullet$ Summary "
+            +"<font color='#888'>"
+            +"&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;"
+            +"&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;"
+            +"&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;&mdash;"
+            +"</font>"
+            +"</h4>")
+        display += "Total counts: "
+        let ilink = 0
+        for(let key in linkCount){
+            let count = linkCount[key]
+            if(count>0){
+                if(ilink>0)
+                    display += ", "
+                display += "$"+latexReformat(key)+"$&times;<font color='blue'>"+count+"</font>"
+            }
+            ilink+=1
+        }
+        display += "<nl>"
+    }
+
+    let notice_text = ""//"<i><font color='blue'>Note: If the result looks strange, the reason might be that the input is ambiguous.</font></i><nl>"
+    document.getElementById("output2").innerHTML = notice_text+display;
     MathJax.typesetPromise();
 }
 
@@ -64,13 +123,31 @@ function latexReformat(latexExpr){
     latexExpr = latexExpr.replaceAll("!=","\\ne ")
     latexExpr = latexExpr.replaceAll("<"," < ")
     latexExpr = latexExpr.replaceAll(">"," > ")
+    latexExpr = latexExpr.replaceAll("("," ( ")
+    latexExpr = latexExpr.replaceAll(")"," ) ")
+    latexExpr = latexExpr.replaceAll("["," [ ")
+    latexExpr = latexExpr.replaceAll("]"," ] ")
     return latexExpr
+}
+
+function extractLinks(input) {
+    const regex = /U(?:\^\\dagger)?_(\\(?:[^\\\s]+)|([^\\\s]))/g;
+    let match;
+    const results = [];
+
+    while ((match = regex.exec(input)) !== null) {
+      // match[0] is the full match, e.g. "U_\\alpha" or "U_b"
+      // You can also check match[1] (the backslash case) or match[2] (the single letter case)
+      results.push(match[0]);
+    }
+
+    return results;
 }
 
 function parseOneTerm(expression){
     
     if(expression==="")
-        return ""
+        return ["",[],""]
 
     // Analyze the summation
     let regex = new RegExp(`\sum_{`, 'g');
@@ -161,10 +238,14 @@ function parseOneTerm(expression){
     const resultList = iterateAll(index_list, condition_dict);
 
     let display = ""
-    if(index_list.length==1)
-        display += "There is 1 loop:"
-    else
-        display += "There are "+index_list.length+" loops:"
+
+    let links = extractLinks(expression)
+    let operator = "\\tr~"
+    for(const link of links){
+        operator += link+" "
+    }
+    operator = latexReformat(operator)
+    display+="Relevant operator: $"+operator+"$<br>"
     display += "<ul>"
     for(const index of index_list){
         display += "<li>"
@@ -176,7 +257,7 @@ function parseOneTerm(expression){
         }else if(condition[0]==="\\leq"){
             display += "$"+index+"$ running from $"+condition_dict[index_list[0]][1]+"$ to $"+condition[1]+"$"
         }else if(condition[0]===">"){
-            display += "$"+index+"$ running from $"+condition[1]+"+1$ to $"+condition_dict[index_list[0]][1]+"$"
+            display += "$"+index+"$ running from $"+condition[1]+"+1$ to $"+condition_dict[index_list[0]][2]+"$"
         }else if(condition[0]==="\\geq"){
             display += "$"+index+"$ running from $"+condition[1]+"$ to $"+condition_dict[index_list[0]][2]+"$"
         }else if(condition[0]==="\\ne"){
@@ -184,13 +265,45 @@ function parseOneTerm(expression){
         }
     }
     display += "</ul>"
-    /*
-    display += "Index values:<br>"
-    for(const index of resultList){
-        display += index+"<br>"
+    
+    let numbered_links = []
+    for(let mu=condition_dict[index_list[0]][1]; mu<condition_dict[index_list[0]][2];mu++){
+        numbered_links.push("U_"+mu)
+        numbered_links.push("U^\\dagger_"+mu)
     }
-    */
-    return display
+
+    let numbered_links_count = []
+    for(const numbered_link of numbered_links)
+        numbered_links_count[numbered_link] = 0
+    
+    for(const index of resultList){
+        let replaced_operator = operator
+        for(let axis=0; axis<index_list.length;axis++){
+            replaced_operator = replaced_operator.replaceAll(index_list[axis],index[axis]+"")
+        }
+        //replaced_operator = latexReformat(replaced_operator)
+        //display += "$"+replaced_operator+"$<br>"
+        for(const numbered_link of numbered_links){
+            numbered_links_count[numbered_link] += (replaced_operator.split(numbered_link).length-1)
+        }
+    }
+    
+    display += "Link variable counts: "
+    let linkCountStr = ""
+    let ilink = 0
+    for(const numbered_link of numbered_links){
+        let count = numbered_links_count[numbered_link]
+        if(count>0){
+            if(ilink>0)
+                linkCountStr += ", "
+            linkCountStr += "$"+latexReformat(numbered_link)+"$&times;"+count
+        }
+        ilink+=1
+    }
+    display += linkCountStr+"<nl>"
+
+    
+    return [display,numbered_links_count,operator+";"+linkCountStr]
 }
 
 function iterateAll(indices, cond) {
@@ -226,33 +339,54 @@ function iterate(indexIdx, indices, cond, current, results, globalBound) {
   const condition = cond[key];
   let low, high;
 
+    function int(x){
+        return parseInt(x,10)
+    }
+
+  function notNumber(variable){
+    return int(variable+"")+""==="NaN"
+  }
+
+  function get_index(variable){
+    if(int(variable+"")+""==="NaN"){
+        return int(current[variable+""])
+    }else{
+        return int(variable+"")
+    }
+  }
+
   if (condition[0] === "=") {
     // fixed range condition
-    low = condition[1];
-    high = condition[2];
+    low = get_index(condition[1])
+    high = get_index(condition[2])
+    
   } else if (condition[0] === "<") {
     const dep = condition[1];
-    if (current[dep] === undefined) return;
-    low = 1;
-    high = parseInt(current[dep],10) - 1;
+    if (current[dep] === undefined && notNumber(dep)) return;
+    low = get_index(1)
+    high = get_index(dep)-1
+
   } else if (condition[0] === "\\leq") {
     const dep = condition[1];
-    if (current[dep] === undefined) return;
-    low = 1;
-    high = parseInt(current[dep],10);
+    if (current[dep] === undefined && notNumber(dep)) return;
+    low = get_index(1)
+    high = get_index(dep)
+
   } else if (condition[0] === ">") {
     const dep = condition[1];
-    if (current[dep] === undefined) return;
-    low = parseInt(current[dep],10) + 1;
-    high = globalBound;
+    if (current[dep] === undefined && notNumber(dep)) return;
+    low = get_index(dep)+1
+    high = get_index(globalBound)
+
   } else if (condition[0] === "\\geq") {
     const dep = condition[1];
-    if (current[dep] === undefined) return;
-    low = parseInt(current[dep],10);
-    high = globalBound;
+    if (current[dep] === undefined && notNumber(dep)) return;
+    low = get_index(dep)
+    high = get_index(globalBound)
+
   } else {
-    low = 1;
-    high = globalBound;
+    low = get_index(1)
+    high = get_index(globalBound)
   }
 
   // If the bounds are not valid, skip this branch.
@@ -263,7 +397,7 @@ function iterate(indexIdx, indices, cond, current, results, globalBound) {
 
     if (condition[0] === "\\ne"){
         const dep = condition[1]
-        if(v==parseInt(current[dep],10))
+        if(v==get_index(dep))
             continue
     }
 
